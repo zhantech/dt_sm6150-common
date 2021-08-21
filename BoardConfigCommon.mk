814cb55e1f94194a5a467cfc7ff97097841f097c
zhantech <hifzhan41@gmail.com>
2021-08-21 15:23:02 +0000

    sm6150-common: build using proton-clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,6 +113,9 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 BOARD_KERNEL_SEPARATED_DTBO := true
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
